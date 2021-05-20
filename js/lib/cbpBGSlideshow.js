25967f9feb20fe24a2c1648d98e964e83bc31f6c
hyerimyang <did3296@naver.com>
2021-05-20 17:04:00 +0900

    js 파일 수정

--- js/lib/cbpBGSlideshow.js
+++ js/lib/cbpBGSlideshow.js
@@ -1,13 +1,3 @@
-/**
- * cbpBGSlideshow.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
 var cbpBGSlideshow = (function() {
 
 	var $slideshow = $( '#cbp-bislideshow' ),
